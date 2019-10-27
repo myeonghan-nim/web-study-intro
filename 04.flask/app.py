8da6d9e5813cadaacfafc8e0b9a72f467d6f5fd9
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-10-27 16:35:28 +0900

    Change: flask files

--- 04.flask/app.py
+++ 04.flask/app.py
@@ -1,19 +1,24 @@
-import csv
-import random
-import requests
-from bs4 import BeautifulSoup
 from flask import Flask, escape, request, render_template
+from bs4 import BeautifulSoup
+import requests
+import random
+import csv
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
+
     return render_template('index.html')
+
 
 @app.route('/lotto')
 def lotto():
     numbers = random.sample(range(1, 46), 6)
+
     return render_template('lotto.html', numbers=numbers)
+
 
 @app.route('/lunch')
 def lunch():
@@ -24,15 +29,21 @@ def lunch():
         '칼국수': 'https://homecuisine.co.kr/files/attach/images/140/824/012/7ece1535875d6ce18990de309b9dbcfb.JPG',
         '나주곰탕': 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2016/1201/IE002061721_STD.JPG'
     }
+
     menu, img = random.choice(list(lst.items()))
+
     return render_template('lunch.html', menu=menu, img=img)
+
 
 @app.route('/opgg')
 def opgg():
+
     return render_template('opgg.html')
+
 
 @app.route('/search')
 def search():
+
     opgg_url = 'https://www.op.gg/summoner/userName='
     summoner = request.args.get('summoner')
     url = opgg_url + summoner
@@ -40,19 +51,23 @@ def search():
     res = requests.get(url).text
     soup = BeautifulSoup(res, 'html.parser')
     tier = soup.select_one('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div > div.TierRankInfo > div.TierRank').text
+
     return render_template('search.html', tier=tier, summoner=summoner)
+
 
 @app.route('/nono')
 def nono():
-
     with open('data.csv', 'r', encoding='utf-8', newline='') as f:
         reader = list(csv.reader(f))
 
     return render_template('nono.html', products=reader)
 
+
 @app.route('/new')
 def new():
+
     return render_template('new.html')
+
 
 @app.route('/create')
 def create():
@@ -67,13 +82,15 @@ def create():
 
     return render_template('create.html')
 
+
 @app.route('/card')
 def card():
-
     with open('data.csv', 'r', encoding='utf-8', newline='') as f:
         reader = list(csv.reader(f))
 
     return render_template('card.html', products=reader)
 
+
 if __name__== '__main__':
     app.run(debug=True)
+    
