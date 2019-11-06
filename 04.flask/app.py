d67937c0c8c1c02d109868484f6242b1a6fb9524
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-11-07 01:12:20 +0900

    Fix: typo

--- 04.flask/app.py
+++ 04.flask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, escape, request, render_template
 from bs4 import BeautifulSoup
+
 import requests
 import random
 import csv
@@ -15,6 +16,7 @@ def hello():
 
 @app.route('/lotto')
 def lotto():
+
     numbers = random.sample(range(1, 46), 6)
 
     return render_template('lotto.html', numbers=numbers)
@@ -22,12 +24,13 @@ def lotto():
 
 @app.route('/lunch')
 def lunch():
+
     lst = {
-        '짜장면': 'http://static.hubzum.zumst.com/hubzum/2019/07/01/10/967a67742c244ebe9ac7c85048c591c3.jpg',
-        '짬뽕': 'https://i.ytimg.com/vi/M04aOUyPIDg/maxresdefault.jpg',
-        '스테이크': 'https://fresheasy.speedgabia.com/contents/p.images/S1932390/750x750.jpg',
-        '칼국수': 'https://homecuisine.co.kr/files/attach/images/140/824/012/7ece1535875d6ce18990de309b9dbcfb.JPG',
-        '나주곰탕': 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2016/1201/IE002061721_STD.JPG'
+        'Black-bean-sauce noodles': 'http://static.hubzum.zumst.com/hubzum/2019/07/01/10/967a67742c244ebe9ac7c85048c591c3.jpg',
+        'Jjambbong': 'https://i.ytimg.com/vi/M04aOUyPIDg/maxresdefault.jpg',
+        'steak': 'https://fresheasy.speedgabia.com/contents/p.images/S1932390/750x750.jpg',
+        'Kalguksu': 'https://homecuisine.co.kr/files/attach/images/140/824/012/7ece1535875d6ce18990de309b9dbcfb.JPG',
+        'Naju gomtang': 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2016/1201/IE002061721_STD.JPG'
     }
 
     menu, img = random.choice(list(lst.items()))
@@ -46,8 +49,8 @@ def search():
 
     opgg_url = 'https://www.op.gg/summoner/userName='
     summoner = request.args.get('summoner')
-    url = opgg_url + summoner
 
+    url = opgg_url + summoner
     res = requests.get(url).text
     soup = BeautifulSoup(res, 'html.parser')
     tier = soup.select_one('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div > div.TierRankInfo > div.TierRank').text
@@ -57,6 +60,7 @@ def search():
 
 @app.route('/nono')
 def nono():
+
     with open('data.csv', 'r', encoding='utf-8', newline='') as f:
         reader = list(csv.reader(f))
 
@@ -71,6 +75,7 @@ def new():
 
 @app.route('/create')
 def create():
+
     product = request.args.get('product')
     category = request.args.get('category')
     replace = request.args.get('replace')
@@ -85,6 +90,7 @@ def create():
 
 @app.route('/card')
 def card():
+    
     with open('data.csv', 'r', encoding='utf-8', newline='') as f:
         reader = list(csv.reader(f))
 
--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# HTML & CSS
+# HTML-CSS
 
-### Web에 친숙해져보자!
-
-- HTML과 CSS를 통해서 web을 구현해서 web에 친숙해져보자! 💻
+- Let`s study about HTML and CSS! 🌐
 
